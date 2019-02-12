68baaa4017e2654ad38d3c09739bc5038d57debc
dotnet050219 <47572589+dotnet050219@users.noreply.github.com>
2019-02-12 21:29:22 +0200

    our second c# program

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // -- this is a remark
+            // System.Console.WriteLine("Hello world!");
+            // asldhgaskjdhgaskjd
+            // askjdhgakjsdh -- one line
+
+            /*
+              1
+              2
+              3
+              multi line 
+             */
+
+
+            int a = 5;
+            int b = a * 66;
+            float f = 5.5f;
+            double d = a * b * f;
+            Console.WriteLine(d);
+
+            
+
+        }
+    }
+}
